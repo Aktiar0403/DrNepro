4b4582599f3293cbafba68e384a4c96e17abc45b
Aktiar0403 <aktiardocuments@gmail.com>
2025-07-17 02:56:37 +0530

    hi

--- js/app.js
+++ js/app.js
@@ -296,8 +296,7 @@ document.getElementById('print-prescription').addEventListener('click', () => {
   printableDiv.style.display = 'block';
   window.print();
   printableDiv.style.display = 'none';
-});
+
 })
-document.getElementById('generate-diagnosis').addEventListener('click', () => {
-  alert('Generate Diagnosis button clicked!');
+
 });
